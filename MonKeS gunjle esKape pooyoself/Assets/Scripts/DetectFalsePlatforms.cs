0b4ba64698d603678884e9baf64fc56e895fb666
Brandon <Brandon2010ajuck@protonmail.ca>
2023-05-09 18:14:18 -0400

    done yas

---
+++ MonKeS gunjle esKape pooyoself/Assets/Scripts/DetectFalsePlatforms.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectFalsePlatforms : MonoBehaviour
+{
+    public bool hit;
+
+    void Update()
+    {
+        hit = Physics.Raycast(transform.position, transform.forward, 10.0f);
+        Debug.DrawRay(transform.position, transform.forward * 10.0f, Color.red);
+
+        if (hit == false)
+        {
+            Debug.LogWarning("Be careful lazy a** bi***");
+        } else
+        {
+            Debug.Log("All clear so lose some wheigh fat a**");
+        }
+    }
+}
