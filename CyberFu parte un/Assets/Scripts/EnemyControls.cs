c81e2309e9ed0023019e4672f2695191cd02cf67
CNMarkham <128920994+Oliviadabest@users.noreply.github.com>
2023-06-13 18:51:36 -0400

    step 8a

---
+++ CyberFu parte un/Assets/Scripts/EnemyControls.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControls : MonoBehaviour
+{
+    public float speed = 5;
+    public float attackingDistance = 1;
+    public Vector3 direction;
+    private Animator animatorEnemy;
+    private Rigidbody rigidbodyEnemy;
+    private Transform target;
+    public bool isFollowingTarget;
+    private bool isAttackingTarget;
+    private float chasingPlayer = 0.01f;
+    public float currentAttackingTime;
+    public float maxAttackingTime = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isFollowingTarget = true;
+        currentAttackingTime = maxAttackingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
