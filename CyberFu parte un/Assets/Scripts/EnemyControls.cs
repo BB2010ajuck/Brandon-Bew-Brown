6a9938e0633e819c4d68bd9ed71bf565d332744a
lee <98993706+MEMEME98@users.noreply.github.com>
2023-06-20 19:00:34 -0400

    done just need to fix

--- CyberFu parte un/Assets/Scripts/EnemyControls.cs
+++ CyberFu parte un/Assets/Scripts/EnemyControls.cs
@@ -11,7 +11,7 @@ public class EnemyControls : MonoBehaviour
     private Rigidbody rigidbodyEnemy;
     private Transform target;
     public bool isFollowingTarget;
-    private bool isAttackingTarget;
+    public bool isAttackingTarget;
     private float chasingPlayer = 0.01f;
     public float currentAttackingTime;
     public float maxAttackingTime = 2f;
@@ -20,11 +20,88 @@ public class EnemyControls : MonoBehaviour
     {
         isFollowingTarget = true;
         currentAttackingTime = maxAttackingTime;
+        animatorEnemy = GetComponentInChildren<Animator>();
+        rigidbodyEnemy = GetComponent<Rigidbody>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Attack();
+    }
+
+    void FollowTarget()
+    {
+        if (!isFollowingTarget)
+        {
+            rigidbodyEnemy.isKinematic = true;
+            return;
+        }
+
+        if (Vector3.Distance(transform.position, target.position) >= attackingDistance)
+        {
+            rigidbodyEnemy.isKinematic = false;
+            direction = target.position - transform.position;
+            direction.y = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 100);
+
+            if (rigidbodyEnemy.velocity.sqrMagnitude != 0)
+            {
+                rigidbodyEnemy.velocity = transform.forward * speed;
+                animatorEnemy.SetBool("Walk", true);
+            }
+        } 
+        else if (Vector3.Distance(transform.position, target.position) <= attackingDistance)
+        {
+            rigidbodyEnemy.isKinematic = false;
+            rigidbodyEnemy.velocity = Vector3.zero;
+            animatorEnemy.SetBool("Walk", true);
+            isFollowingTarget = false;
+            isAttackingTarget = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        FollowTarget();
+    }
+    
+    void Attack()
+    {
+        if (!isAttackingTarget)
+        {
+            return;
+        }
+
+        currentAttackingTime += Time.deltaTime;
+
+        if (currentAttackingTime > maxAttackingTime)
+        {
+            currentAttackingTime = 0f;
+            EnemyAttack(Random.Range(1, 4));
+        }
+
+        if (Vector3.Distance(transform.position, target.position) > attackingDistance + chasingPlayer)
+        {
+            isFollowingTarget = true;
+            isAttackingTarget = false;
+        }
+    }
+
+    public void EnemyAttack(int attack)
+    {
+        if (attack == 1)
+        {
+            animatorEnemy.SetTrigger("Attack1");
+        }
+        if (attack == 2)
+        {
+            animatorEnemy.SetTrigger("Attack2");
+        }
+        if (attack == 3)
+        {
+            animatorEnemy.SetTrigger("Attack3");
+        }
     }
 }
