a0f9767a29fc50e88503b605cf2837e20f044e52
Roosham <markhamonca@codeninjas.com>
2023-08-08 18:57:31 -0400

    prove yoself

---
+++ BrownBaybe Nenga worLds 1 I 3/Assets/HUD.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+    private GameObject[] hearts;
+    private int lives = 3;
+    public GameObject background;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hearts = GameObject.FindGameObjectsWithTag("heart");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HurtPlayer()
+    {
+        Debug.Log("Damn Mother###### That Hurts");
+        lives -= 1;
+        hearts[lives].SetActive(false);
+        if (lives == 0)
+        {
+            background.GetComponent<GameManager>().GameOver();
+        }
+    }
+
+    
+}
