6210fad7f306f8ce4965de8e33f8087545da075c
Roosham <127261958+BB2010ajuck@users.noreply.github.com>
2023-08-09 17:53:05 -0400

    Just need Teacher to check my work then prove yoself

---
+++ World of rainbow pride/Assets/Scripts/LevelReset.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelReset : MonoBehaviour
+{
+    public GameObject player;
+    public ParticleSystem explosion;
+    public void GameOver()
+    {
+        player.SetActive(false);
+        Invoke("Reload", 2);
+        explosion.Play();
+    }
+
+    void Reload()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void Start()
+    {
+        explosion.Stop();
+    }
+
+    private void FixedUpdate()
+    {
+        explosion.transform.position = player.transform.position;
+    }
+}
+
+
