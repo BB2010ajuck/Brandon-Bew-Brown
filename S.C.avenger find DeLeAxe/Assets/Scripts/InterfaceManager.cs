ed4bad07f54916149037094a3f344d96d065787a
Griffin <markhamonca@codeninjas.com>
2023-09-19 18:57:51 -0400

    Im on 4d

---
+++ S.C.avenger find DeLeAxe/Assets/Scripts/InterfaceManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class InterfaceManager : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public TextMeshPro dialogText;
+    public Image seekImage;
+    public GameObject npc;
+    public GameObject randomSpawn;
+
+    [SerializeField]
+    private Sprite[] collectibleSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CollectibleUpdate(int item)
+    {
+
+    }
+
+    public void ShowBox(string dialog, int item)
+    {
+        if (npc.GetComponent<DialogOpen>().begin)
+        {
+            scatterCoins();
+        }
+    }
+
+    public void scatterCoins()
+    {
+        randomSpawn.GetComponent<RandomSpawn>().DistributeCollectibles();
+        npc.GetComponent<DialogOpen>().coinsScattered();
+    }
+}
